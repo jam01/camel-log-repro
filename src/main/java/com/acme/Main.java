4a1bc5b772a97ecc0c20f8313165e7f834dbc851
jam01 <jam01@protonmail.com>
2025-10-16 14:02:27 -0500

    swallow exc

--- src/main/java/com/acme/Main.java
+++ src/main/java/com/acme/Main.java
@@ -6,6 +6,10 @@ public class Main extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        onException(Exception.class)
+                .logHandled(true)
+                .handled(true);
+
         from("timer:foo?period=1000")
                 .setBody(header("foo"))
                 .throwException(new IllegalStateException())
